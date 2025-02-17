377455bfe6d4240643400aaecf7e4d9530df5717
DiegoMayorga <diegoamayorgat@gmail.com>
2025-02-17 10:44:01 -0500

    Switching the hook to useRouteLoaderData to use the loader function to both EventDetail and EditEvent pages.

--- frontend/src/pages/EventDetail.jsx
+++ frontend/src/pages/EventDetail.jsx
@@ -1,39 +1,15 @@
-/* import { Link , useParams  } from "react-router-dom"; */
-import { useLoaderData } from "react-router-dom";
+import { useRouteLoaderData } from "react-router-dom";
 import EventItem from "../components/EventItem";
 
 export default function EventDetailPage() {
-  /* const params = useParams(); */
-  const data = useLoaderData();
+  const data = useRouteLoaderData('event-detail');
 
-  return (
-    <EventItem event={data.event} />
-    /* <>
-      <h1>Event Detail Page</h1>
-      <p>Event ID: {params.eventId}</p>
-      <p>
-        <Link to=".." relative="path">
-          Back
-        </Link>
-      </p>
-      <p>
-        <Link to="edit">Edit</Link>
-      </p>
-    </> */
-  );
+  return <EventItem event={data.event} />;
 }
-// objeto request.
-// params: objeto con todos mis parametros de ruta.
+
 export async function loader({ request, params }) {
-  //aqui no usaremos request porque no es necesario. Ese request es para temas de url.
-  // params es lo que se necesita para completar la url de abajo.
   const id = params.eventId;
-
   const response = await fetch("http://localhost:8080/events/" + id);
-
-  // o tambien se puede asi:
-  // return await fetch('http://localhost:8080/events/' + id);
-  // pero como quiero verificar si el !response.ok, uso el otro enfoque.
 
   if (!response.ok) {
     throw new Error(
