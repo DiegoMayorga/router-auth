b3763cfe6ddb595a94c49b9816d4e24ef0a30556
DiegoMayorga <diegoamayorgat@gmail.com>
2025-02-18 12:22:11 -0500

    Adding the delete method and fetching the updated data.

--- frontend/src/pages/EventDetail.jsx
+++ frontend/src/pages/EventDetail.jsx
@@ -1,8 +1,8 @@
-import { useRouteLoaderData } from "react-router-dom";
+import { redirect, useRouteLoaderData } from "react-router-dom";
 import EventItem from "../components/EventItem";
 
 export default function EventDetailPage() {
-  const data = useRouteLoaderData('event-detail');
+  const data = useRouteLoaderData("event-detail");
 
   return <EventItem event={data.event} />;
 }
@@ -12,13 +12,29 @@ export async function loader({ request, params }) {
   const response = await fetch("http://localhost:8080/events/" + id);
 
   if (!response.ok) {
-    throw new Error(
-      { message: "Could not fetch details for selected event." },
-      {
-        status: 500,
-      }
+    throw new Response(
+      JSON.stringify(
+        { error: "Could not fetch details for selected event." },
+        {
+          status: 500,
+        }
+      )
     );
   } else {
     return response;
   }
+}
+
+export async function action({ params, request }) {
+  const eventId = params.eventId;
+  const response = await fetch("http://localhost:8080/events/" + eventId, {
+    method: request.method,
+  });
+
+  if (!response.ok) {
+    throw new Response(JSON.stringify({ error: "Could not delete event." }), {
+      status: 500,
+    });
+  }
+  return redirect("/events");
 }
